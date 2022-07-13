7d29ff5db976981fac458fc6943fc7ee8d395007
Bhorn Ironside <tromesh6143@gmail.com>
2022-07-13 08:47:40 -0400

    added new components and features

--- src/features/restaurants/components/restaurant-info.js
+++ src/features/restaurants/components/restaurant-info.js
@@ -1,45 +1,95 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Card, Paragraph } from "react-native-paper";
-import styled from "styled-components";
+import { View, Text, Image } from "react-native";
+import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
 
-const Title = styled.Text`
-  color: palevioletred;
-  margin: 10px;
+import { Spacer } from "../../../components/spacer/spacer.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/isOpen";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]}
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body}
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Info = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[1]}
+  padding-bottom: ${(props) => props.theme.space[1]}
+`;
+
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     address = "100 random street",
-    openingHours = true,
+    isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  console.log(ratingArray);
+
   return (
     <>
-      <View>
-        <Card elevation={5} style={styles.card}>
-          <Card.Cover
-            key={name}
-            style={styles.cardCover}
-            source={{ uri: photos }}
-          />
+      <RestaurantCard elevation={5}>
+        <RestaurantCardCover key={name} source={{ uri: photos }} />
+        <Info>
           <Title>{name}</Title>
-        </Card>
-      </View>
+          <Section>
+            <Rating>
+              {ratingArray.map(() => (
+                <SvgXml xml={star} width={20} height={20} />
+              ))}
+            </Rating>
+            <SectionEnd>
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: "red" }}>
+                  Closed Temporarily
+                </Text>
+              )}
+              <Spacer variant="left.large" />
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              <Spacer variant="left.large" />
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </SectionEnd>
+          </Section>
+          <Address>{address}</Address>
+        </Info>
+      </RestaurantCard>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "white",
-  },
-  cardCover: {
-    padding: 10,
-    backgroundColor: "white",
-  },
-});
